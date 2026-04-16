f5a465687a817a3a4b44366fda347954f135a508
ranjithks002 <ranjithks002@gmail.com>
2026-04-16 17:19:50 +0530

    Add UserRepository interface for user data access

---
+++ src/main/java/com.example.bookingSystem/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.bookingSystem.repository;
+
+import com.example.bookingSystem.model.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<Users, Integer> {
+    Optional<Users> findByEmail(String email);
+}
